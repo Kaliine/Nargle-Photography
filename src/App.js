b730944f3276069ffcdd8e9e1fc352c9d5e6f56f
Kaliine <80845418+Kaliine@users.noreply.github.com>
2021-12-28 20:26:27 +0100

    project set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+
+import { Navbar } from './components/Navbar'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        {/* <Switch>
+          <Route />
+        </Switch> */}
+      </BrowserRouter>
+    </>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = (props) => {
+
+  /* const menuItems = [
+
+  ] */
+
+  return ( 
+    <div className="navbar">
+      <h1 className="header-title">Nargle Photography</h1>
+    </div>
+  )
+}
+
+export { Navbar }
