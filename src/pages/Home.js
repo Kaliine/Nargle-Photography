8fdd90c29aac187d6d2b8631c80acb50c326e68e
Kaliine <80845418+Kaliine@users.noreply.github.com>
2023-01-15 13:28:00 +0100

    added styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ import img from 'assets/bg-image.jpg'
 import toothIcon from '../assets/icons8-dental-64.png'
 import tfLogo from '../assets/pic1.jpg'
 import patient from '../assets/patient.jpg'
-import { Navbar } from '../components/Navbar'
 
 /* Hero section */
 const HeroImg = styled.div`
@@ -25,6 +24,10 @@ const HeroImgTextWrapper = styled.div`
 text-align: center;
 max-width: 600px;
 margin-left: 100px;
+@media (max-width: 768px) {
+  max-width: 400px;
+  margin-left: 30px;
+}
 `
 const HeroImgTextBig = styled.h1`
   font-size: 66px;
@@ -32,6 +35,9 @@ const HeroImgTextBig = styled.h1`
   font-family: 'Inter';
   margin-bottom: 10px;
   letter-spacing: 1px;
+  @media (max-width: 768px) {
+  font-size: 44px;
+}
   `
 const Btn = styled.button`
 background-color: #73C89A;
@@ -55,6 +61,7 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
+overflow: hidden;
 `
 const Section = styled.section`
 display: flex;
@@ -62,6 +69,7 @@ flex-direction: column;
 justify-content: center;
 align-items: center;
 width: 100%;
+margin-top: 40px;
 `
 const PinkTextWrapper = styled.div`
 display: flex;
@@ -70,6 +78,12 @@ align-items: center;
 color: #FDC5C6;
 font-size: 36px;
 max-width: 750px;
+margin-bottom: 40px;
+@media (max-width: 768px) {
+  font-size: 26px;
+  padding: 0 20px;
+  text-align: center;
+}
 `
 const HeadingWrapper = styled.div`
 display: flex;
@@ -77,25 +91,50 @@ color: #73C89A;
 font-size: 30px;
 width: 100%;
 margin-left: 80px;
+margin-bottom: 20px;
+@media (max-width: 768px) {
+  font-size: 26px;
+}
 `
 const TextContainer = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: flex-start;
 color: #FDC5C6;
-font-size: 40px;
 width: 60%;
+@media (max-width: 768px) {
+  width: 80%;
+}
 `
 const Wrapper= styled.div`
 display: flex;
 justify-content: flex-start;
-align-items: center;
-margin: -10px;
+align-items: top;
+margin-bottom: 15px;
 `
+const ToothImg= styled.img`
+margin-right: 15px;
+margin-top: 8px;
+@media (max-width: 768px) {
+  margin-top: 5px;
+}
+`
+
 const ServicesParagraph= styled.p`
-margin-left: 40px;
 color: #000;
 font-size: 24px;
+@media (max-width: 768px) {
+  font-size: 18px;
+}
+`
+const ServicesParagraphBold= styled.p`
+color: #000;
+font-size: 24px;
+font-weight: bold;
+margin-top: 15px;
+@media (max-width: 768px) {
+  font-size: 18px;
+}
 `
 const ImgWrapper= styled.div`
 margin-top: 50px;
@@ -105,16 +144,31 @@ const AboutParagraphBold= styled.p`
 color: #000;
 font-size: 24px;
 font-weight: bold;
+@media (max-width: 768px) {
+  font-size: 18px;
+}
 `
 const AboutParagraph= styled.p`
 color: #000;
 font-size: 24px;
+@media (max-width: 768px) {
+  font-size: 18px;
+}
+`
+const LastAboutParagraph= styled.p`
+color: #000;
+font-size: 24px;
+margin-top: 15px;
+@media (max-width: 768px) {
+  font-size: 18px;
+}
 `
 const LogoContainer= styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
-margin-bottom: 60px;
+margin-top: 40px;
+margin-bottom: 40px;
 `
 /* Contact section */
 const ContactSection = styled.section`
@@ -125,6 +179,7 @@ align-items: center;
 width: 100%;
 background-color: #73C89A;
 padding-bottom: 60px;
+padding-top: 40px;
 `
 const ContactHeadingWrapper = styled.div`
 display: flex;
@@ -132,6 +187,10 @@ color: #fff;
 font-size: 30px;
 width: 100%;
 margin-left: 80px;
+margin-bottom: 20px;
+@media (max-width: 768px) {
+  font-size: 26px;
+}
 `
 const ContactTextContainer = styled.div`
 display: flex;
@@ -142,6 +201,17 @@ text-align: center;
 const ContactParagraph= styled.p`
 color: #fff;
 font-size: 24px;
+@media (max-width: 768px) {
+  font-size: 18px;
+}
+`
+const LastContactParagraph= styled.p`
+color: #fff;
+font-size: 24px;
+margin-bottom: 20px;
+@media (max-width: 768px) {
+  font-size: 18px;
+}
 `
 
 const Home = () => {
@@ -168,93 +238,50 @@ const Home = () => {
             </HeadingWrapper>
             <TextContainer>
               <Wrapper>
-                <div>
-                  <img src={toothIcon} alt="Ikon tand" height="24" width="24" />
-                </div>
-                <div>
+                  <ToothImg src={toothIcon} alt="Ikon tand" height="20" width="20" />
                   <ServicesParagraph>Undersökning med röntgenbilder</ServicesParagraph>
-                </div>
               </Wrapper>
               <Wrapper>
-                <div>
-                  <img src={toothIcon} alt="Ikon tand" height="24" width="24" />
-                </div>
-                <div>
+                  <ToothImg src={toothIcon} alt="Ikon tand" height="20" width="20" />
                   <ServicesParagraph>Registrering av karies och parodontit (tandlossning) samt kontroll av munslemhinnor och bettförhållande. Avvikelser rapporteras till ansvarig tandläkare.</ServicesParagraph>
-                </div>
               </Wrapper>
               <Wrapper>
-                <div>
-                  <img src={toothIcon} alt="Ikon tand" height="24" width="24" />
-                </div>
-                <div>
+                  <ToothImg src={toothIcon} alt="Ikon tand" height="20" width="20" />
                   <ServicesParagraph>Individuell information och instruktion om hur man behåller sin munhälsa.</ServicesParagraph>
-                </div>
               </Wrapper>
               <Wrapper>
-                <div>
-                  <img src={toothIcon} alt="Ikon tand" height="24" width="24" />
-                </div>
-                <div>
+                  <ToothImg src={toothIcon} alt="Ikon tand" height="20" width="20" />
                   <ServicesParagraph>Förebyggande behandling av karies, framförallt med olika fluorbehandlingsbehandlingsmetoder.</ServicesParagraph>
-                </div>
               </Wrapper>
               <Wrapper>
-                <div>
-                  <img src={toothIcon} alt="Ikon tand" height="24" width="24" />
-                </div>
-                <div>
+                  <ToothImg src={toothIcon} alt="Ikon tand" height="20" width="20" />
                   <ServicesParagraph>Förebyggande och behandling av tandlossningssjukdomar.</ServicesParagraph>
-                </div>
               </Wrapper>
               <Wrapper>
-                <div>
-                  <img src={toothIcon} alt="Ikon tand" height="24" width="24" />
-                </div>
-                <div>
+                  <ToothImg src={toothIcon} alt="Ikon tand" height="20" width="20" />
                   <ServicesParagraph>Borttagning av tandsten.</ServicesParagraph>
-                </div>
               </Wrapper>
               <Wrapper>
-                <div>
-                  <img src={toothIcon} alt="Ikon tand" height="24" width="24" />
-                </div>
-                <div>
+                  <ToothImg src={toothIcon} alt="Ikon tand" height="20" width="20" />
                   <ServicesParagraph>Polering av tänder samt puts av fyllningar.</ServicesParagraph>
-                </div>
               </Wrapper>
               <Wrapper>
-                <div>
-                  <img src={toothIcon} alt="Ikon tand" height="24" width="24" />
-                </div>
-                <div>
+                  <ToothImg src={toothIcon} alt="Ikon tand" height="20" width="20" />
                   <ServicesParagraph>Tandblekning för hemmabruk och klinikblekning.</ServicesParagraph>
-                </div>
               </Wrapper>
               <Wrapper>
-                <div>
-                  <img src={toothIcon} alt="Ikon tand" height="24" width="24" />
-                </div>
-                <div>
+                  <ToothImg src={toothIcon} alt="Ikon tand" height="20" width="20" />
                   <ServicesParagraph>Receptföreskrivning på flourpreparat.</ServicesParagraph>
-                </div>
               </Wrapper>
               <Wrapper>
-                <div>
-                  <img src={toothIcon} alt="Ikon tand" height="30" width="30" />
-                </div>
-                <div>
-                  <ServicesParagraph>Har du frågor angående priser, hör gärna av dig till mig!</ServicesParagraph>
-                </div>
+                  <ServicesParagraphBold>Har du frågor angående priser, hör gärna av dig till mig!</ServicesParagraphBold>
               </Wrapper>
               <Wrapper>
-                <div>
                   <ServicesParagraph>Jag är ansluten till Försäkringskassan och har naturligtvis såväl patient- som ansvarsförsäkring. Behandling hos mig ligger till grund för ditt högkostnadsskydd.</ServicesParagraph>
-                </div>
               </Wrapper>
             </TextContainer>
             <ImgWrapper>
-              <img src={patient} alt="Leende patient" height="auto" width="1400" />
+              <img src={patient} alt="Leende patient" height="auto" width="1400"/>
             </ImgWrapper>
           </Section>
           <Section id="about">
@@ -268,23 +295,23 @@ const Home = () => {
                   <br />Jag tog min tandhygienistexamen 1988 i Stockholm och har nu över 35 års erfarenhet i branschen. Jag har arbetat både inom folktandvården och privat. Sedan 25 år är jag egen företagare.
                   <br />Som tandhygienist är jag specialutbildad i munnens friskvård. Hos mig kan du få hjälp med allt från förebyggande vård såsom skräddarsydda råd och tips om hur du bäst ska sköta dina tänder, till tandstensborttagning, puts, polering och blekning.
                 </AboutParagraph>
-                <AboutParagraph>Medlem i Sveriges Tandhygienistförening och Sveriges TandhygienistFöretagare.</AboutParagraph>
+                <LastAboutParagraph>Medlem i Sveriges Tandhygienistförening och Sveriges TandhygienistFöretagare.</LastAboutParagraph>
               </div>
               <LogoContainer>
-                <img src={tfLogo} alt="Logga Sveriges Tandhygienistförening" height="100" widht="100" />
+                <img src={tfLogo} alt="Logga Sveriges Tandhygienistförening" height="100" widht="100" id="contact" />
               </LogoContainer>
             </TextContainer>
           </Section>
-          <ContactSection id="contact">
+          <ContactSection>
             <ContactHeadingWrapper>
               <h2>Kontakt</h2>
             </ContactHeadingWrapper>
             <ContactTextContainer>
               <div>
-                <ContactParagraph>Ring: 0176 - 509 92</ContactParagraph>
-                <ContactParagraph>Mejla: ca.andersson@live.se</ContactParagraph>
-                <ContactParagraph>Besök: Grisslehamnsvägen 1, Väddö</ContactParagraph>
-                <ContactParagraph>Öppettider: mån, tis, tor, fre</ContactParagraph>
+                <ContactParagraph>Telefon: 0176 - 509 92</ContactParagraph>
+                <ContactParagraph>E-post: ca.andersson@live.se</ContactParagraph>
+                <ContactParagraph>Adress: Grisslehamnsvägen 1, Väddö</ContactParagraph>
+                <LastContactParagraph>Öppet: mån, tis, tor, fre</LastContactParagraph>
               </div>
               <iframe width="100%" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=grisslehamnsv%C3%A4gen%201&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
             </ContactTextContainer>
